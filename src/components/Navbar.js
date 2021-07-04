d2552c0b9d062e6b99f61ef3b9682a6ccdd262f8
Lazir Pascual <lazirpascual@gmail.com>
2021-07-04 17:47:25 -0400

    add navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,18 +2,51 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 // Material-UI import
-import AppBar from "@material-ui/core/AppBar";
-import Toolbar from "@material-ui/core/Toolbar";
-import Typography from "@material-ui/core/Typography";
+import { AppBar, Toolbar, makeStyles, Typography } from "@material-ui/core";
+import LocalMallOutlinedIcon from "@material-ui/icons/LocalMallOutlined";
+import IconButton from "@material-ui/core/IconButton";
+import MenuIcon from "@material-ui/icons/Menu";
+import Box from "@material-ui/core/Box";
+
+const useStyles = makeStyles((theme) => ({
+  header: {
+    backgroundColor: "inherit",
+    color: "black",
+    boxShadow: "0px 0px 0px 0px",
+  },
+  spacing: {
+    fontSize: "20px",
+    margin: 25,
+    textDecoration: "none",
+    color: "inherit",
+  },
+  shop: {
+    [theme.breakpoints.up("sm")]: {
+      marginLeft: theme.spacing(1),
+      width: "auto",
+    },
+  },
+}));
 
 const Navbar = () => {
+  const classes = useStyles();
+
   return (
     <div>
-      <AppBar position="fixed">
+      <AppBar position="fixed" className={classes.header}>
         <Toolbar>
-          <Link to="/">Home</Link>
-          <Link to="/shop">Shop</Link>
-          <Link to="/cart">Cart</Link>
+          <IconButton edge="start" color="inherit" aria-label="menu">
+            <MenuIcon />
+          </IconButton>
+          <Link to="/" className={classes.spacing}>
+            Cloud Computers
+          </Link>
+          <Link to="/shop" className={classes.spacing}>
+            Shop
+          </Link>
+          <Link to="/cart" className={classes.spacing}>
+            <LocalMallOutlinedIcon />
+          </Link>
         </Toolbar>
       </AppBar>
       <Toolbar />
