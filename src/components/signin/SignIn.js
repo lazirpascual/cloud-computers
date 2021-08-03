785ec44d53d1f2f0737402c1b2df92ef2d10fc69
Lazir Pascual <lazirpascual@gmail.com>
2021-08-03 19:04:27 -0400

    separate SignInForm into its own component

--- src/components/signin/SignIn.js
+++ src/components/signin/SignIn.js
@@ -1,17 +1,10 @@
-import React, { useState, useContext } from "react";
-import { UserContext } from "../../contexts/UserContext";
-import { useHistory } from "react-router-dom";
+import React, { useState } from "react";
 import Notification from "../helper/Notification";
 import Copyright from "../helper/Copyright";
+import SignInForm from "./SignInForm";
 
 import Avatar from "@material-ui/core/Avatar";
-import Button from "@material-ui/core/Button";
 import CssBaseline from "@material-ui/core/CssBaseline";
-import TextField from "@material-ui/core/TextField";
-import FormControlLabel from "@material-ui/core/FormControlLabel";
-import Checkbox from "@material-ui/core/Checkbox";
-import Link from "@material-ui/core/Link";
-import Grid from "@material-ui/core/Grid";
 import Box from "@material-ui/core/Box";
 import LockOutlinedIcon from "@material-ui/icons/LockOutlined";
 import Typography from "@material-ui/core/Typography";
@@ -20,30 +13,14 @@ import useStyles from "./styles";
 
 export default function SignIn() {
   const classes = useStyles();
-  const [username, setUsername] = useState("");
-  const [password, setPassword] = useState("");
-  const { loginToApp } = useContext(UserContext);
-  const history = useHistory();
-
   const [open, setOpen] = useState(false);
   const [errorMessage, setErrorMessage] = useState("sdf");
 
-  const handleLogin = async (event) => {
-    event.preventDefault();
-
-    try {
-      const loginSuccess = await loginToApp(username, password);
-      setUsername("");
-      setPassword("");
-      if (loginSuccess) {
-        history.push(`/`);
-      }
-    } catch (exception) {
-      const errorMessage = `Invalid Username or Password`;
-      setOpen(true);
-      setErrorMessage(errorMessage);
-      console.log(errorMessage);
-    }
+  const handleException = (exception) => {
+    const errorMessage = `Invalid Username or Password`;
+    setOpen(true);
+    setErrorMessage(errorMessage);
+    console.log(exception);
   };
 
   return (
@@ -57,63 +34,7 @@ export default function SignIn() {
         <Typography component="h1" variant="h5">
           Sign in
         </Typography>
-        <form onSubmit={handleLogin} className={classes.form} noValidate>
-          <TextField
-            onChange={(e) => setUsername(e.target.value)}
-            value={username}
-            variant="outlined"
-            margin="normal"
-            required
-            fullWidth
-            id="username"
-            label="Username"
-            name="username"
-            autoComplete="username"
-            autoFocus
-          />
-          <TextField
-            onChange={(e) => setPassword(e.target.value)}
-            value={password}
-            variant="outlined"
-            margin="normal"
-            required
-            fullWidth
-            name="password"
-            label="Password"
-            type="password"
-            id="password"
-            autoComplete="current-password"
-          />
-          <FormControlLabel
-            control={<Checkbox value="remember" color="primary" />}
-            label="Remember me"
-          />
-          <Button
-            type="submit"
-            fullWidth
-            variant="contained"
-            color="primary"
-            className={classes.submit}
-          >
-            Sign In
-          </Button>
-          <Grid container>
-            <Grid item xs>
-              <Link href="#" variant="body2">
-                Forgot password?
-              </Link>
-            </Grid>
-            <Grid item>
-              <Link
-                className={classes.cursor}
-                onClick={() => history.push(`/signup`)}
-                variant="body2"
-              >
-                {"Don't have an account? Sign Up"}
-              </Link>
-            </Grid>
-          </Grid>
-        </form>
+        <SignInForm handleException={handleException} />
       </div>
       <Box mt={8}>
         <Copyright />
---
+++ src/components/signin/SignInForm.js
@@ -0,0 +1,92 @@
+import React, { useState, useContext } from "react";
+import { UserContext } from "../../contexts/UserContext";
+import { useHistory } from "react-router-dom";
+
+import { Button, Link, Grid, TextField } from "@material-ui/core";
+import { FormControlLabel, Checkbox } from "@material-ui/core";
+import useStyles from "./styles";
+
+const SignInForm = ({ handleException }) => {
+  const classes = useStyles();
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const { loginToApp } = useContext(UserContext);
+  const history = useHistory();
+
+  const handleLogin = async (event) => {
+    event.preventDefault();
+
+    try {
+      const loginSuccess = await loginToApp(username, password);
+      setUsername("");
+      setPassword("");
+      if (loginSuccess) {
+        history.push(`/`);
+      }
+    } catch (exception) {
+      handleException(exception);
+    }
+  };
+
+  return (
+    <form onSubmit={handleLogin} className={classes.form} noValidate>
+      <TextField
+        onChange={(e) => setUsername(e.target.value)}
+        value={username}
+        variant="outlined"
+        margin="normal"
+        required
+        fullWidth
+        id="username"
+        label="Username"
+        name="username"
+        autoComplete="username"
+        autoFocus
+      />
+      <TextField
+        onChange={(e) => setPassword(e.target.value)}
+        value={password}
+        variant="outlined"
+        margin="normal"
+        required
+        fullWidth
+        name="password"
+        label="Password"
+        type="password"
+        id="password"
+        autoComplete="current-password"
+      />
+      <FormControlLabel
+        control={<Checkbox value="remember" color="primary" />}
+        label="Remember me"
+      />
+      <Button
+        type="submit"
+        fullWidth
+        variant="contained"
+        color="primary"
+        className={classes.submit}
+      >
+        Sign In
+      </Button>
+      <Grid container>
+        <Grid item xs>
+          <Link href="#" variant="body2">
+            Forgot password?
+          </Link>
+        </Grid>
+        <Grid item>
+          <Link
+            className={classes.cursor}
+            onClick={() => history.push(`/signup`)}
+            variant="body2"
+          >
+            {"Don't have an account? Sign Up"}
+          </Link>
+        </Grid>
+      </Grid>
+    </form>
+  );
+};
+
+export default SignInForm;
