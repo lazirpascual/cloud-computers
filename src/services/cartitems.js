2a3fda09acae5bc532c4d869b3f19ca86f28d663
Lazir Pascual <lazirpascual@gmail.com>
2021-08-04 12:52:44 -0400

    switch cartitems services methods from Promises to Async/Await

--- src/services/cartitems.js
+++ src/services/cartitems.js
@@ -2,24 +2,24 @@ import axios from "axios";
 const baseUrl = "https://cloudcomputers.herokuapp.com/api/cartitems";
 // const baseUrl = "/api/cartitems";
 
-const getAll = () => {
-  const request = axios.get(baseUrl);
-  return request.then((response) => response.data);
+const getAll = async () => {
+  const request = await axios.get(baseUrl);
+  return request.data;
 };
 
-const create = (newObject) => {
-  const request = axios.post(baseUrl, newObject);
-  return request.then((response) => response.data);
+const create = async (newObject) => {
+  const request = await axios.post(baseUrl, newObject);
+  return request.data;
 };
 
-const update = (id, newObject) => {
-  const request = axios.put(`${baseUrl}/${id}`, newObject);
-  return request.then((response) => response.data);
+const update = async (id, newObject) => {
+  const request = await axios.put(`${baseUrl}/${id}`, newObject);
+  return request.data;
 };
 
-const remove = (id) => {
-  const request = axios.delete(`${baseUrl}/${id}`);
-  return request.then((response) => response.data);
+const remove = async (id) => {
+  const request = await axios.delete(`${baseUrl}/${id}`);
+  return request.data;
 };
 
 const cartService = { getAll, create, update, remove };
