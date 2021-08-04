116d1e6b5924e903dfde0fbd83a9225189ed8aa6
Lazir Pascual <lazirpascual@gmail.com>
2021-08-04 01:00:55 -0400

    add ProductCard unit test

---
+++ src/tests/ProductCard.test.js
@@ -0,0 +1,24 @@
+import React from "react";
+import "@testing-library/jest-dom/extend-expect";
+import { render } from "@testing-library/react";
+import ProductCard from "../components/shop/ProductCard";
+import ProductContextProvider from "../contexts/ProductContext";
+
+test("<ProductCard /> renders the correct content", () => {
+  const product = {
+    name: "AMD Ryzen 5 5600X",
+    price: 299.99,
+    imgPath: "https://i.imgur.com/XVadBTJ.png",
+    imgPreview: "https://i.imgur.com/53XOJCb.jpg",
+  };
+
+  const component = render(
+    <ProductContextProvider>
+      <ProductCard product={product} />
+    </ProductContextProvider>
+  );
+
+  // check if the rendered ProductCard contains the correct content
+  expect(component.container).toHaveTextContent("AMD Ryzen 5 5600X");
+  expect(component.container).toHaveTextContent(299.99);
+});
